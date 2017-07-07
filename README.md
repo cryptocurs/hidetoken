811150076f5df0dfc4c75a9e2bdf4a6119a4b5b4
cryptocoins <crypto-coins@yandex.ru>
2017-07-07 13:12:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Hidetoken Smart Contract
---
+++ hidetoken.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.4;
+
+contract Hidetoken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    
+    mapping (address => uint256) public balanceOf;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    function Hidetoken(uint256 _supply, string _name, string _symbol, uint8 _decimals) {
+        if (_supply == 0) _supply = 1036799900000000;
+        
+        balanceOf[msg.sender] = _supply;
+        name = _name;
+        symbol = _symbol;
+        
+        decimals = _decimals;
+    }
+    
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        Transfer(msg.sender, _to, _value);
+    }
+}
